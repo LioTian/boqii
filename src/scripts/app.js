2a4ff302902d9e1e229d3824e1c5a1e1faf2e1f6
Fiona <924940874@qq.com>
2016-08-21 15:58:03 +0800

    project.0821

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+require("./views/zepto.min.js");
+require("./views/swiper-3.3.1.min.js");
 require("./libs/spa.min.js");
 require("./views/index.js");
-
--- src/scripts/views/index.js
+++ src/scripts/views/index.js
@@ -1,5 +1,22 @@
 var indexTpl = require("../tpls/index.string");
 //定义视图
 SPA.defineView("index",{
-	html:indexTpl
+	html:indexTpl,
+
+	//轮播图
+	bindEvents:{
+		"show":function(){
+			var mySwiper = new Swiper ('.swiper-container', {
+				autoplay:2000,
+				autoplayDisableOnInteraction : false,
+				
+			    direction: 'horizontal',
+			    loop: true,
+			    // 如果需要分页器
+			    pagination: '.swiper-pagination',
+			  })
+		}
+	}
+
+
 })
